91f84cc4b6f85365ee66f6cbf85b0c7258b2b44e
Jeff Reygers <jareygers@gmail.com>
2020-03-31 01:49:50 -0400

    added text

--- JS101/room.js
+++ JS101/room.js
@@ -9,4 +9,6 @@ let width = readLineSync.prompt();
 let squareMeters = length * width;
 let squareFeet = (squareMeters * 10.739).toFixed(2);
 
-console.log(`${squareMeters} (${squareFeet} square feet)`);
+console.log(
+  `The area of the room is ${squareMeters} square meters (${squareFeet} square feet)`
+);
