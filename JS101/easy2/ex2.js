76e03d27e9f3b2f27002c4a128b679612b1ed35e
Jeff Reygers <jareygers@gmail.com>
2020-04-02 02:24:19 -0400

    removed ! character from name variable

--- JS101/easy2/ex2.js
+++ JS101/easy2/ex2.js
@@ -3,6 +3,7 @@ let readLineSync = require("readline-sync");
 let name = readLineSync.question("What is your name? ");
 
 if (name.charAt(name.length - 1) === "!") {
+  name = name.slice(0, -1);
   console.log(`HELLO ${name.toLocaleUpperCase()}. WHY ARE WE SCREAMING?`);
 } else {
   console.log(`Hello ${name}`);
